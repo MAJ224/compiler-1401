2da559d75108c30123e3ec0ce669aa192bdd830a
MAJ224 <aminjavan22478@gmail.com>
2022-04-23 17:28:42 +0430

    init commit

---
+++ src/compilerproject/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilerproject;
+
+import GUI.MainFrame;
+
+/**
+ *
+ * @author Arash
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Data.MF = new MainFrame();
+        Data.MF.setVisible(true);
+    }
+    
+}
